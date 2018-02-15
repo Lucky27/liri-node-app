5029f63ff90c480238d16deddf8e81efe7ddd6d9
Carlo Manalese <CarloManalese@calvisitor-10-105-151-144.calvisitor.1918.berkeley.edu>
2018-02-14 17:29:15 -0800

    tweeter data

--- liri.js
+++ liri.js
@@ -1,23 +1,33 @@
 
-var dotenv = require ("dotenv");
-var spotify = require("node-spotify-api");
-var client = require("spotify");
+require("dotenv").config();
+var nodeSpotify = require("node-spotify-api");
+var spotify = require("spotify");
 var request = require("request");
-var twitter = require("twitter");
-var keys = require ("keys.js");
+var Twitter = require("twitter");
+var keys = require ("./keys.js"); 
+var userInput = process.argv;
 
 function getTweet(){
- // var client = new Twitter(keys.twitter);
-	
+var client = new Twitter(keys.twitter);
+
 var params = {
-	screen_name:'barrack obama', 
-	count:20};
+  screen_name:'DrakeReact'
+};
+
 
 client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  	if (!error) {
-    console.log(tweets);
-  }
+    if (!error) { 
+      for(var i = 0; i<20; i++){
+        console.log(i);
+        console.log(tweets[i].text);
+        console.log(tweets[i].created_at);
+      }
+    }
+    // else{
+    //   console.log("error", error)
+    // }
 });
 }
 
-getTweet()
+getTweet(); 
+
