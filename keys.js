bacee4d6b4af87357f0173f3838f1cb2814780b2
Carlo Manalese <CarloManalese@calvisitor-10-105-150-91.calvisitor.1918.berkeley.edu>
2018-02-15 18:22:44 -0800

    twitter, do what it says, ombd

--- keys.js
+++ keys.js
@@ -1,14 +1,31 @@
 
-console.log('this is loaded');
+// console.log('this is loaded');
+
+// exports.twitter = {
+//   consumer_key: process.env.TWITTER_CONSUMER_KEY,
+//   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+//   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+//   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+// };
+
+// exports.spotify = {
+//   id: process.env.SPOTIFY_ID,
+//   secret: process.env.SPOTIFY_SECRET
+// };
+
+console.log("It's linked");
 
 exports.twitter = {
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+	consumer_key: process.env.TWITTER_CONSUMER_KEY,
+	consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+	access_token_key: process .env.TWITTER_ACCESS_TOKEN_KEY,
+	access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+	access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
 
 exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
+
+	id: process.env.SPOTIFY_ID,
+	secret: process.env.SPOTIFY_SECRET
+
 };
--- liri.js
+++ liri.js
@@ -1,33 +1,151 @@
 
+// require("dotenv").config();
+// var nodeSpotify = require("node-spotify-api");
+// var spotify = require("spotify");
+// var request = require("request");
+// var Twitter = require("twitter");
+// var keys = require ("./keys.js"); 
+// var userInput = process.argv;
+
+// function getTweet(){
+// var client = new Twitter(keys.twitter);
+
+// var params = {
+//   screen_name:"GreenDay"
+// };
+
+
+// client.get('statuses/user_timeline', params, function(error, tweets, response) {
+//     if (!error) { 
+//       for(var i = 0; i<20; i++){
+//         console.log(i);
+//         console.log(tweets[i].text);
+//         console.log(tweets[i].created_at);
+//       }
+//     }
+//     else{
+//       console.log("error", error)
+//     }
+// });
+// }
+
+// getTweet(); 
+
 require("dotenv").config();
 var nodeSpotify = require("node-spotify-api");
-var spotify = require("spotify");
+var Spotify = require("spotify");
 var request = require("request");
 var Twitter = require("twitter");
-var keys = require ("./keys.js"); 
+var keys = require ("./keys.js");
 var userInput = process.argv;
-
-function getTweet(){
-var client = new Twitter(keys.twitter);
-
-var params = {
-  screen_name:'DrakeReact'
-};
+var fs = require("fs");
 
 
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-    if (!error) { 
+function grabTweet (){
+  var client = new Twitter(keys.twitter);
+
+  var params = {
+    screen_name: "hansonmusic"
+  };
+
+  client.get("statuses/user_timeline", params, function(error, tweets, response){
+    if (!error){
       for(var i = 0; i<20; i++){
         console.log(i);
-        console.log(tweets[i].text);
         console.log(tweets[i].created_at);
+        console.log(tweets[i].text);
+
       }
     }
-    // else{
-    //   console.log("error", error)
-    // }
-});
+  });
 }
 
-getTweet(); 
+grabTweet()
+
+
+  // spotify.search({ type: 'track', query: 'changes' }, function(err, data) {
+  //   if ( err ) {
+  //       console.log('Error occurred: ' + err);
+  //       return;
+  //   }
+  //   else{
+  //     console.log(data);
+  //   }
+  // spotify.search({ type: 'track', query: "changes" })
+  // .then(function(response) {
+  //   console.log(response);
+  // })
+  // .catch(function(err) {
+  //   console.log(err);
+  // })
+ //     * Artist(s)
+     
+     // * The song's name
+     
+     // * A preview link of the song from Spotify
+     
+     // * The album that the song is from
+    // Do something with 'data' 
+// });
+
+// }
+
+// getSong()
+
+
+//getMovie function starts
+  function getMovie(){
+
+  var title = process.argv[3];
+  request("http://www.omdbapi.com/?t="+title+"&y=&plot=short&apikey=trilogy", function(error, response, body){
+    if(!error && response.statusCode === 200){
+      console.log("The title of the movie: " + JSON.parse(body).Title);
+      console.log("Year the movie came out: " + JSON.parse(body).Year);
+      console.log("IMBD Rating of the movie: " + JSON.parse(body).imdbRating);
+      console.log("Rotten Tomatoes Rating of the movie: " + JSON.parse(body).Ratings[1].Value);
+      console.log("Country where the movie was produced: " + JSON.parse(body).Country);
+      console.log("Language of the movie: " + JSON.parse(body).Language);
+      console.log("Plot of the movie: " + JSON.parse(body).Plot);
+      console.log("Actors in the movie" + JSON.parse(body).Actors);
+    }
+
+  });
+ 
+}
+getMovie()
+  
+//   if (userInput[2] === "movie-this"){
+//     getMovie()
+//   }
+
+//readFile function start
+function readFile(){
+  fs.readFile("random.text", "utf8", function(err, data){
+    if(err){
+      console.log(err)
+    }
+    console.log(data);
+  });
+}
+
+readFile()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
